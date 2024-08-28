0b87091f22b2544044275c576c53e664d045084a
Martelo <gabriel.martelosso@gmail.com>
2024-08-28 19:02:16 -0300

    Fix decision tree algorithm and include test

--- DecisionTree.py
+++ DecisionTree.py
@@ -2,15 +2,15 @@ import numpy as np
 from collections import Counter
 
 class Node:
-    def __ini__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
+    def __init__(self, feature=None, threshold=None, left=None, right=None,*,value=None):
         self.feature = feature
         self.threshold = threshold
         self.left = left
         self.right = right
-        self.value=None
-    
+        self.value = value
+
     def is_leaf_node(self):
-        return self.value is not None
+        return self.value is not None 
 
 
 
@@ -23,23 +23,24 @@ class DecisionTree:
         self.root=None
 
 
-    def fit(self):
-        self.n_features = X.shape[1] if not self.n_features else min(X.shape[1], self.n_features)
+    def fit(self, X, y):
+        self.n_features = X.shape[1] if not self.n_features else min(X.shape[1],self.n_features)
         self.root = self._grow_tree(X, y)
 
-    def _grow_tree(self, X, y, depth):
+
+    def _grow_tree(self, X, y, depth=0):
         n_samples, n_feats = X.shape
         n_labels = len(np.unique(y))
 
-        # Checa o criterio de parada
+        # Checa o critério de parada
         if (depth >= self.max_depth or n_labels == 1 or n_samples < self.min_samples_split):
             leaf_value = self._most_common_label(y)
             return Node(value=leaf_value)
 
-        feat_idx = np.random.choice(n_feats, self.n_features, replace=False)
+        feat_idxs = np.random.choice(n_feats, self.n_features, replace=False)
 
-        # Encontra melhores divisoes
-        best_feature, best_thresh = self._best_split(X, y, feat_idx)
+        # Encontra melhores divisões
+        best_feature, best_thresh = self._best_split(X, y, feat_idxs)
 
         # Cria nos filhos
         left_idxs, right_idxs = self._split(X[:, best_feature], best_thresh)
@@ -51,7 +52,7 @@ class DecisionTree:
         best_gain = -1
         split_idx, split_threshold = None, None
 
-        for feat_idx, in feat_idxs:
+        for feat_idx in feat_idxs:
             X_column = X[:, feat_idx]
             thresholds = np.unique(X_column)
 
@@ -80,22 +81,22 @@ class DecisionTree:
         n = len(y)
         n_l, n_r = len(left_idxs), len(right_idxs)
         e_l, e_r = self._entropy(y[left_idxs]), self._entropy(y[right_idxs])
-        child_entropy = (n_l / n) * e_l + (n_r / n) * e_r
+        child_entropy = (n_l/n) * e_l + (n_r/n) * e_r
 
         # Calcula o information gain
         information_gain = parent_entropy - child_entropy
         return information_gain
 
     def _split(self, X_column, split_thresh):
-        left_idxs = np.agrwhere(X_column <= split_thresh).flatten()
-        right_idxs = np.agrwhere(X_column > split_thresh).flatten()
+        left_idxs = np.argwhere(X_column <= split_thresh).flatten()
+        right_idxs = np.argwhere(X_column > split_thresh).flatten()
         return left_idxs, right_idxs
 
 
     def _entropy(self, y):
         hist = np.bincount(y)
         ps = hist / len(y)
-        return np.sum([p * np.log2(p) for p in ps if p > 0])
+        return -np.sum([p * np.log(p) for p in ps if p>0])
 
     def _most_common_label(self, y):
         counter = Counter(y)
@@ -107,7 +108,7 @@ class DecisionTree:
 
     def _traverse_tree(self, x, node):
         if node.is_leaf_node():
-            return node.value()
+            return node.value
 
         if x[node.feature] <= node.threshold:
             return self._traverse_tree(x, node.left)
